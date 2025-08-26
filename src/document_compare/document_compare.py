d0cc832605058406a4f726a6a35349a3232c1134
kartekjadhav <kartekjadhav2810.com>
2025-08-26 07:30:56 +0000

    Completed document comparator module, testing is left

--- src/document_compare/document_compare.py
+++ src/document_compare/document_compare.py
@@ -22,22 +22,28 @@ class DocumentCompareLLM():
         self.chain = self.prompt | self.llm | self.fixing_parser
         self.log.info("DocumentCompareLLM initialized successfully.")
 
-    def compare_documents(self):
+    def compare_documents(self, combined_doc:str) -> pd.DataFrame:
         """
         Compare two documents in a structured way.
         """
         try:
-            pass
+            response = self.chain.invoke({
+                "format_instructions": self.fixing_parser.get_format_instructions(),
+                "document_text_combined": combined_doc
+            })
+            return self._format_response(response)
+
         except Exception as e:
             self.log.error("Error in comparing documents", error=str(e))
             DocumentPortalException("An error occured while comparing documents.", sys)
 
-    def _format_response(self):
+    def _format_response(self, response_parsed: list[dict]) -> pd.DataFrame:
         """
         Format the response from the LLM in a structured way.
         """
         try:
-            pass
+            df = pd.DataFrame(response_parsed)
+            return df
         except Exception as e:
             self.log.error("Error in formatting the response", error=str(e))
             DocumentPortalException("An error occured while formatting the response.", sys)
--- src/document_compare/document_ingestion.py
+++ src/document_compare/document_ingestion.py
@@ -1,5 +1,7 @@
 import os
 import sys
+from datetime import datetime
+from uuid import uuid4
 from pathlib import Path
 import fitz
 from logger.custom_logger import CustomLogger
@@ -14,12 +16,28 @@ class DocumentComparator:
         self.base_dir.mkdir(parents=True, exist_ok=True)
         self.log.info("DocumentComparator initialized successfully.")
 
-    def save_uploaded_file(self):
+    def save_uploaded_file(self, reference_file, actual_file):
         """
         Save the uploaded file to a specific directory.
         """
         try:
-            pass
+            self.delete_existing_files()
+            
+            reference_file_path = self.base_dir / reference_file
+            actual_file_path = self.base_dir / actual_file
+
+            if not reference_file.endswith(".pdf") or not actual_file.endwith(".pdf"):
+                raise ValueError("Only PDF files are allowed.")
+
+            with open(reference_file_path, "wb") as f:
+                f.write(reference_file.getbuffer())
+
+            with open(actual_file_path, "wb") as f:
+                f.write(actual_file.getbuffer())
+
+            self.log.info("Successuly saved uploaded files.", reference_file_path={str(reference_file_path)}, actual_file_path={str(actual_file_path)})
+            return reference_file_path, actual_file_path
+
         except Exception as e:
             self.log.error("Error in saving uploaded file", error=str(e))
             DocumentPortalException("Error in saving uploaded file", sys)
@@ -39,7 +57,7 @@ class DocumentComparator:
                     if text:
                         doc_text.append(f"\n\n-------Page {page_num}-------\n\n{text}")
                 self.log.info(f"PDF successfully read from {pdf_path}")
-                return "".join(doc_text)
+                return "\n".join(doc_text)
 
         except Exception as e:
             self.log.error("Error in reading uploaded file", error=str(e))
@@ -50,7 +68,32 @@ class DocumentComparator:
         Delete the existing files at specific paths.
         """
         try:
-            pass
+            if self.base_dir.exists() and self.base_dir.is_dir():
+                for file in self.base_dir.iterdir():
+                    if file.is_file():
+                        file.unlink()
+                        self.log.info(f"Deleted existing file: {file}")
+                self.log.info("Existing files deleted successfully.")
         except Exception as e:
             self.log.error("Error in deleting uploaded file", error=str(e))
             DocumentPortalException("Error in deleting uploaded file", sys)
+
+    
+    def conbine_documents(self):
+        try:
+            docs_part  = []
+            content = {}
+            for file in self.base_dir.iterdir():
+                if file.is_file() and file.suffix == ".pdf":
+                    content[file.name] = self.read_pdf(file)
+            
+            for filename, data in content.items():
+                docs_part.append(f"Document: {filename}\n\n{data}\n\n")
+            
+            combined_text = "\n".join(docs_part)
+            return combined_text
+
+
+        except Exception as e:
+            self.log.error("Error occuered while combining the files")
+            DocumentPortalException("Error occuered while combining the files", sys)
