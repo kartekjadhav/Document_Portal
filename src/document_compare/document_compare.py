65c4bf656e85db82b126e4b882df0207dba34c3a
kartekjadhav <kartekjadhav2810.com>
2025-08-27 06:59:37 +0000

    Completed document_compare.py

--- src/document_compare/document_compare.py
+++ src/document_compare/document_compare.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 from logger.custom_logger import CustomLogger
 from exception.custom_exception import DocumentPortalException
 from utils.model_loader import ModelLoader
-import pandas as pd
-from models.models import *
+from prompt.prompt_library import PROMPT_REGISTRY
 from langchain_core.output_parsers import JsonOutputParser
 from langchain.output_parsers import OutputFixingParser
-from prompt.prompt_library import PROMPT_REGISTRY
+from models.models import *
+import pandas as pd
 
 
 class DocumentCompareLLM():
@@ -20,30 +20,30 @@ class DocumentCompareLLM():
         self.fixing_parser = OutputFixingParser(parser=self.parser, llm=self.llm)
         self.prompt = PROMPT_REGISTRY["document_comparison"]
         self.chain = self.prompt | self.llm | self.fixing_parser
-        self.log.info("DocumentCompareLLM initialized successfully.")
+        self.log.info("DocumentCompareLLM successfully initialised.")
 
-    def compare_documents(self, combined_doc:str) -> pd.DataFrame:
+    def compare_documents(self, combined_docs:str) -> pd.DataFrame:
         """
         Compare two documents in a structured way.
         """
         try:
             response = self.chain.invoke({
                 "format_instructions": self.fixing_parser.get_format_instructions(),
-                "document_text_combined": combined_doc
+                "document_text_combined": combined_docs
             })
+
             return self._format_response(response)
 
         except Exception as e:
             self.log.error("Error in comparing documents", error=str(e))
             DocumentPortalException("An error occured while comparing documents.", sys)
 
-    def _format_response(self, response_parsed: list[dict]) -> pd.DataFrame:
+    def _format_response(self, response_parsed) -> pd.DataFrame:
         """
         Format the response from the LLM in a structured way.
         """
         try:
-            df = pd.DataFrame(response_parsed)
-            return df
+            return pd.DataFrame(response_parsed)
         except Exception as e:
             self.log.error("Error in formatting the response", error=str(e))
             DocumentPortalException("An error occured while formatting the response.", sys)
